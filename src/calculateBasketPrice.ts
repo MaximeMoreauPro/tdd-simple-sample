b00a1ac869afaf9a1aff90d99b1f4610dcb77e06
MaximeMoreauPro <117015980+MaximeMoreauPro@users.noreply.github.com>
2023-08-07 08:48:56 +0100

    refacto: externalise the unit price handling in the getItemUnitPrice function

--- src/calculateBasketPrice.ts
+++ src/calculateBasketPrice.ts
@@ -2,16 +2,19 @@ export function calculateBasketPrice(items: string[]): number {
     let basketPrice = 0;
 
     for(let i = 0; i < items.length; i++) {
-        if(items[i] === 'Apple') {
-            basketPrice += 0.35;
-        } else if(items[i] === 'Banana') {
-            basketPrice += 0.2;
-        } else if(items[i] === 'Melon') {
-            basketPrice += 0.5;
-        } else if(items[i] === 'Lime') {
-            basketPrice += 0.15;
-        }
+        basketPrice += getItemUnitPrice(items[i]);
     }
 
     return basketPrice;
+}
+
+function getItemUnitPrice(item: string) {
+    const itemUnitPrices = new Map<string, number>([
+        ['Apple', 0.35],
+        ['Banana', 0.2],
+        ['Melon', 0.5], 
+        ['Lime', 0.15]]
+    );
+
+    return itemUnitPrices.get(item) || 0;
 }
