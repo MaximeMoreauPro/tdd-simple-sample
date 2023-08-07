f6a00ea94a8227d583292ef8ea5786b39a45e7f7
MaximeMoreauPro <117015980+MaximeMoreauPro@users.noreply.github.com>
2023-08-07 08:55:01 +0100

    test: add a Rule describe bloc "Rule: should calculate the basket price by items quantity and unit price"

--- src/calculateBasketPrice.test.ts
+++ src/calculateBasketPrice.test.ts
@@ -1,27 +1,33 @@
 import { calculateBasketPrice } from './calculateBasketPrice';
 
 describe('calculateBasketPrice function that calculates the price of a basket of shopping', () => {    
-    it('should return 0 when no items are passed', () => {
-        expect(calculateBasketPrice([])).toBe(0);
-    })
+    describe('Rule: should calculate the basket price by items quantity and unit price', () => { 
+        it('should return 0 when no items are passed', () => {
+            expect(calculateBasketPrice([])).toBe(0);
+        })
 
-    it('should return 0.35 when there is an apple in the basket', () => {
-        expect(calculateBasketPrice(['Apple'])).toBe(0.35);
-    })
+        it('should return 0.35 when there is an apple in the basket', () => {
+            expect(calculateBasketPrice(['Apple'])).toBe(0.35);
+        })
 
-    it('should return 0.7 when there are two apples in the basket', () => {
-        expect(calculateBasketPrice(['Apple', 'Apple'])).toBe(0.7);
-    })
+        it('should return 0.7 when there are two apples in the basket', () => {
+            expect(calculateBasketPrice(['Apple', 'Apple'])).toBe(0.7);
+        })
 
-    it('should return 0.4 when there are two bananas in the basket', () => {
-        expect(calculateBasketPrice(['Banana', 'Banana'])).toBe(0.4);
-    })
+        it('should return 0.4 when there are two bananas in the basket', () => {
+            expect(calculateBasketPrice(['Banana', 'Banana'])).toBe(0.4);
+        })
 
-    it('should return 1 when there are two melons in the basket', () => {
-        expect(calculateBasketPrice(['Melon', 'Melon'])).toBe(1);
-    })
+        it('should return 1 when there are two melons in the basket', () => {
+            expect(calculateBasketPrice(['Melon', 'Melon'])).toBe(1);
+        })
 
-    it('should return 0.3 when there are two limes in the basket', () => {
-        expect(calculateBasketPrice(['Lime', 'Lime'])).toBe(0.3);
+        it('should return 0.3 when there are two limes in the basket', () => {
+            expect(calculateBasketPrice(['Lime', 'Lime'])).toBe(0.3);
+        })
+
+        it('should return 2.4 when there are two apples, two bananas, two melons and two limes in the basket', () => {
+            expect(calculateBasketPrice(['Apple', 'Banana', 'Melon', 'Lime', 'Apple', 'Banana', 'Melon', 'Lime'])).toBe(2.4);
+        })
     })
 });
